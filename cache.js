61d10dbcaead6f6e1bf68912035e6d0503c899e5
Shane Lacey <shanelacey@Shanes-Mac-mini.local>
2016-05-20 16:14:29 +0100

    Linted, changed test file to use lab and code instead of mocha and assert

--- cache.js
+++ cache.js
@@ -1,120 +1,117 @@
-/* Copyright (c) 2014 Seamus D'Arcy, MIT License */
-"use strict";
+'use strict'
 
-var LRU = require('lru-cache');
+var LRU = require('lru-cache')
 
-module.exports = function(options) {
-  var seneca = this;
-  var plugin   = 'cache';
+module.exports = function (options) {
+  var seneca = this
+  var plugin = 'cache'
 
   options = seneca.util.deepextend({
     lrucache: {
-      max:    9999,
+      max: 9999,
       maxAge: 1000 * 60 * 60,
-      length: function(n) { return n.length },
+      length: function (n) { return n.length }
     }
-  }, options);
+  }, options)
 
 
-  var cache = LRU(options.lrucache);
+  var cache = LRU(options.lrucache)
 
 
   // cache patterns
-  seneca.add({role: plugin, cmd: 'set'},    cmd_set);
-  seneca.add({role: plugin, cmd: 'get'},    cmd_get);
-  seneca.add({role: plugin, cmd: 'add'},    cmd_add);
-  seneca.add({role: plugin, cmd: 'delete'}, cmd_delete);
-  seneca.add({role: plugin, cmd: 'incr'},   incrdecr('incr'));
-  seneca.add({role: plugin, cmd: 'decr'},   incrdecr('decr'));
+  seneca.add({role: plugin, cmd: 'set'}, cmd_set)
+  seneca.add({role: plugin, cmd: 'get'}, cmd_get)
+  seneca.add({role: plugin, cmd: 'add'}, cmd_add)
+  seneca.add({role: plugin, cmd: 'delete'}, cmd_delete)
+  seneca.add({role: plugin, cmd: 'incr'}, incrdecr('incr'))
+  seneca.add({role: plugin, cmd: 'decr'}, incrdecr('decr'))
 
-  seneca.add({role: plugin, get: 'native'},   get_native );
+  seneca.add({role: plugin, get: 'native'}, get_native)
 
   // lru-cache specific patterns
-  seneca.add({role: 'lrucache', cmd: 'peek'},   cmd_peek);
-  seneca.add({role: 'lrucache', cmd: 'reset'},  cmd_reset);
-  seneca.add({role: 'lrucache', cmd: 'has'},    cmd_has);
-  seneca.add({role: 'lrucache', cmd: 'keys'},   cmd_keys);
-  seneca.add({role: 'lrucache', cmd: 'values'}, cmd_values);
+  seneca.add({role: 'lrucache', cmd: 'peek'}, cmd_peek)
+  seneca.add({role: 'lrucache', cmd: 'reset'}, cmd_reset)
+  seneca.add({role: 'lrucache', cmd: 'has'}, cmd_has)
+  seneca.add({role: 'lrucache', cmd: 'keys'}, cmd_keys)
+  seneca.add({role: 'lrucache', cmd: 'values'}, cmd_values)
 
 
-  function cmd_set(args, done) {
-    var key = args.key;
-    var val = args.val;
-    cache.set(key, val);
-    done(null, key);
+  function cmd_set (args, done) {
+    var key = args.key
+    var val = args.val
+    cache.set(key, val)
+    done(null, key)
   };
 
-  function cmd_get(args, done) {
-    var key = args.key;
-    var val = cache.get(key);
-    done(null, val);
+  function cmd_get (args, done) {
+    var key = args.key
+    var val = cache.get(key)
+    done(null, val)
   };
 
-  function cmd_add(args, done) {
-    var key = args.key;
-    var val = args.val;
+  function cmd_add (args, done) {
+    var key = args.key
+    var val = args.val
     if (!cache.has(key)) {
-      cache.set(key, val);
+      cache.set(key, val)
     }
-    else return done(new Error('key exists: '+key))
+    else return done(new Error('key exists: ' + key))
 
-    done(null, key);
+    done(null, key)
   };
 
-  function cmd_delete(args, done) {
-    cache.del(args.key);
-    done(null, args.key);
+  function cmd_delete (args, done) {
+    cache.del(args.key)
+    done(null, args.key)
   };
 
 
-  function incrdecr(kind) {
-    return function(args, done) {
-      var key = args.key;
-      var val = args.val;
+  function incrdecr (kind) {
+    return function (args, done) {
+      var key = args.key
+      var val = args.val
 
-      var oldVal = cache.get(key);
-      if ( null == oldVal ) return done(null);
+      var oldVal = cache.get(key)
+      if (null == oldVal) return done(null)
       if (typeof oldVal !== 'number') {
-        return done(new Error(kind + ' failed - value for key ' + key + ' is not a number'));
+        return done(new Error(kind + ' failed - value for key ' + key + ' is not a number'))
       }
-      var newVal = kind === 'decr' ? oldVal - val : oldVal + val;
+      var newVal = kind === 'decr' ? oldVal - val : oldVal + val
 
-      cache.set(key, newVal);
-      done(null, newVal);
+      cache.set(key, newVal)
+      done(null, newVal)
     }
   }
 
 
-  function get_native(args, done) {
-    done(null,cache)
+  function get_native (args, done) {
+    done(null, cache)
   }
 
-
-  function cmd_peek(args, done) {
-    var val = cache.peek(args.key);
-    done(null, val);
+  function cmd_peek (args, done) {
+    var val = cache.peek(args.key)
+    done(null, val)
   };
 
-  function cmd_reset(args, done) {
-    cache.reset();
-    done(null);
+  function cmd_reset (args, done) {
+    cache.reset()
+    done(null)
   };
 
-  function cmd_has(args, done) {
-    var has = cache.has(args.key);
-    done(null, has);
+  function cmd_has (args, done) {
+    var has = cache.has(args.key)
+    done(null, has)
   };
 
-  function cmd_keys(args, done) {
-    var keys = cache.keys();
-    done(null, keys);
+  function cmd_keys (args, done) {
+    var keys = cache.keys()
+    done(null, keys)
   };
 
-  function cmd_values(args, done) {
-    var values = cache.values();
-    done(null, values);
+  function cmd_values (args, done) {
+    var values = cache.values()
+    done(null, values)
   };
 
-
-  return {name: plugin};
-};
+  return {name: plugin}
+}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,87 +1,90 @@
-/* Copyright (c) 2014 Seamus D'Arcy */
-/*global describe, it */
-"use strict";
+'use strict'
 
-var seneca = require('seneca')({log:'silent'});
-seneca.use('..');
+var seneca = require('seneca')({log: 'silent'})
+seneca.use('..')
 
-var assert = require('assert');
+var Lab = require('lab')
+var Code = require('code')
 
-var standard = require('seneca-cache-test')
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
+var it = lab.it
+var expect = Code.expect
 
-describe('cache', function() {
+var Standard = require('seneca-cache-test')
 
-  it('basic',function(done){
-    standard.basictest(seneca,done)
+describe('cache', function () {
+  it('basic', function (done) {
+    Standard.basictest(seneca, done)
   })
 
 
-  var cache = seneca.pin({role: 'cache', cmd: '*'});
-  var lrucache = seneca.pin({role: 'lrucache', cmd: '*'});
+  var cache = seneca.pin({role: 'cache', cmd: '*'})
+  var lrucache = seneca.pin({role: 'lrucache', cmd: '*'})
 
 
-  it('set', function(fin) {
-    cache.set({key: 'x', val: '10'}, function(err, out) {
-      if(err) return fin(err);
+  it('set', function (fin) {
+    cache.set({key: 'x', val: '10'}, function (err, out) {
+      if (err) return fin(err)
 
-      cache.set({key: 'y', val: 20}, function(err, out) {
-        fin(err);
+      cache.set({key: 'y', val: 20}, function (err, out) {
+        fin(err)
       })
     })
   })
 
 
-  it('peek', function(fin) {
-    lrucache.peek({key: 'x'}, function(err, out) {
-      if(err) return fin(err);
-      assert.equal(out, '10');
+  it('peek', function (fin) {
+    lrucache.peek({key: 'x'}, function (err, out) {
+      if (err) return fin(err)
+      expect(out).to.equal('10')
 
-      lrucache.peek({key: 'y'}, function(err, out) {
-        if(err) return fin(err);
-        assert.equal(out, 20);
+      lrucache.peek({key: 'y'}, function (err, out) {
+        if (err) return fin(err)
+        expect(out).to.equal(20)
 
-        fin();
-      });
-    });
-  });
-
-
-  it('has', function(fin) {
-    lrucache.has({key: 'x'}, function(err, out) {
-      if(err) return fin(err);
-      assert(out);
-      fin();
-    });
-  });
+        fin()
+      })
+    })
+  })
 
 
-  it('keys', function(fin) {
-    lrucache.keys({}, function(err, out) {
-      if(err) return fin(err);
-      assert.deepEqual(out.sort(), ['x', 'y']);
-      fin();
-    });
-  });
+  it('has', function (fin) {
+    lrucache.has({key: 'x'}, function (err, out) {
+      if (err) return fin(err)
+      expect(out)
+      fin()
+    })
+  })
 
 
-  it('values', function(fin) {
-    lrucache.values({}, function(err, out) {
-      if(err) return fin(err);
-      assert.deepEqual(out.sort(), ['10', 20]);
-      fin();
-    });
-  });
-
-  it('reset', function(fin) {
-    lrucache.reset({}, function(err, out) {
-      if(err) return fin(err);
-      lrucache.keys({}, function(err, out) {
-        if(err) return fin(err);
-        assert.equal(out.length, 0);
-        fin();
-      });
-    });
-  });
+  it('keys', function (fin) {
+    lrucache.keys({}, function (err, out) {
+      if (err) return fin(err)
+      expect(out).to.include(['x', 'y'])
+      expect(out).to.have.length(2)
+      fin()
+    })
+  })
 
 
-});
+  it('values', function (fin) {
+    lrucache.values({}, function (err, out) {
+      if (err) return fin(err)
+      expect(out).to.include(['10', 20])
+      expect(out).to.have.length(2)
+      fin()
+    })
+  })
+
+  it('reset', function (fin) {
+    lrucache.reset({}, function (err, out) {
+      if (err) return fin(err)
+      lrucache.keys({}, function (err, out) {
+        if (err) return fin(err)
+        expect(out.length).to.equal(0)
+        fin()
+      })
+    })
+  })
+})
