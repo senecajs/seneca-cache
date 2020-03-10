382f73a456764ee60776b92029524a210534ba6c
rjrodger <richard@ricebridge.com>
2020-03-10 15:35:32 +0000

    action level micro cache

--- cache.js
+++ cache.js
@@ -1,9 +1,20 @@
-/* Copyright (c) 2014-2019 Richard Rodger, Seamus D'Arcy, and other contributors, MIT License. */
+/* Copyright (c) 2014-2020 Richard Rodger, Seamus D'Arcy, and other contributors, MIT License. */
 'use strict'
 
-var LRU = require('lru-cache')
+const LRU = require('lru-cache')
+const Jsonic = require('jsonic')
+
+const Micro = require('./lib/micro')
+
 
 module.exports = cache
+
+module.exports.defaults = {
+  micro: {
+    expiry: 11111 // 10-ish seconds
+  }
+}
+
 module.exports.errors = {
   key_exists: 'Key <%=key%> exists.',
   op_failed_nan:
@@ -28,7 +39,8 @@ function cache(options) {
   )
 
   var cache = new LRU(options.lrucache)
-
+  var micro = []
+  
   // cache patterns
   seneca.add({ role: plugin, cmd: 'set' }, cmd_set)
   seneca.add({ role: plugin, cmd: 'get' }, cmd_get)
@@ -40,6 +52,11 @@ function cache(options) {
 
   seneca.add({ role: plugin, get: 'native' }, get_native)
 
+
+  seneca.add({ role: plugin, cmd: 'micro' }, cmd_micro)
+  seneca.add({ role: plugin, cmd: 'micro', get:'stats' }, get_micro_stats)
+  
+  
   // lru-cache specific patterns
   seneca.add({ role: 'lrucache', cmd: 'peek' }, cmd_peek)
   seneca.add({ role: 'lrucache', cmd: 'reset' }, cmd_reset)
@@ -47,6 +64,10 @@ function cache(options) {
   seneca.add({ role: 'lrucache', cmd: 'keys' }, cmd_keys)
   seneca.add({ role: 'lrucache', cmd: 'values' }, cmd_values)
 
+
+
+
+  
   function cmd_set(msg, reply) {
     var key = msg.key
     var val = msg.val
@@ -142,5 +163,48 @@ function cache(options) {
     reply({ values: values })
   }
 
-  return { name: plugin }
+
+  function cmd_micro(msg, reply) {
+    var seneca = this
+    var expiry = msg.expiry || options.micro.expiry
+    // TODO: should be provided by Seneca.util
+    var pins = msg.pin || msg.pins
+    pins = Array.isArray(pins) ? pins :
+      'string' === typeof(pins) ? pins.split(';').map(pin=>Jsonic(pin)) :
+      [Object.assign({},pins)]
+
+    pins.forEach((pin)=>{
+      var m = Micro({expiry:expiry})
+      micro.push({m:m,pin:seneca.util.pattern(pin)})
+      seneca.root.wrap(pin, function micro_cache(msg,reply) {
+        m.submit(
+          (callback)=>{
+            this.prior(msg,callback)
+          },
+          (...res)=>{
+            reply(...res)
+          }
+        )
+      })
+    })
+    
+    reply({
+      pins: pins
+    })
+  }
+
+
+  function get_micro_stats(msg, reply) {
+    var stats = []
+    micro.forEach(m=>{
+      var s = Object.assign({pin:m.pin},m.m.stats())
+      s.total = s.miss + s.hit
+      s.hr = s.hit / s.total
+      s.lr = s.live / s.total
+      stats.push(s)
+    })
+    reply(stats)
+  }
+  
+
 }
